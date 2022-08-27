b5e34d841b56d00347b70841cff0a7330b2ae3f0
Moazzam19 <moazzam@syntaxtechs.com>
2022-08-27 11:07:03 -0400

    final code for the class

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -4,8 +4,6 @@ public class gitBasics {
     public static void main(String[] args) {
 //        write down a code that checks the temperature from a website and then
 //        prints out that is it a beautiful day or not
-        System.out.println("today is a not beautiful day");
-        System.out.println("today is a not beautiful day");
-        System.out.println("today is a not beautiful day");
+        System.out.println("hello batch13 people");
     }
 }
